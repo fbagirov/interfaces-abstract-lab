d252bc8e5a07404b5baa6b590dcb56da980dc3df
pl200x <pw2663@columbia.edu>
2026-03-09 19:23:31 -0400

    Create Motorcycle.java

---
+++ src/transport/Motorcycle.java
@@ -0,0 +1,27 @@
+package transport;
+
+public class Motorcycle extends Vehicle {
+    private int speed;
+    private boolean hasSidecar;
+    private String color;
+    public Motorcycle(String brand, int year, int speed, boolean hasSidecar, String color) {
+        super(brand, year);  
+        this.speed = speed;
+        this.hasSidecar = hasSidecar;
+        this.color = color;
+    }
+    @Override
+    public void move() {
+        System.out.println(brand + " motorcycle is moving at " + speed + " mph.");
+    }
+    @Override
+    public int getSpeed() {
+        return speed;
+    }
+    public void displayMotorcycleInfo() {
+        displayInfo(); 
+        System.out.println("Speed: " + speed);
+        System.out.println("Has sidecar: " + hasSidecar);
+        System.out.println("With color: " + color);
+    }
+}
