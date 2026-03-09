63963c353dd5f0471f30766e73513b8dfe4c31a3
Minqi Huang <mh4650@columbia.edu>
2026-03-09 19:45:30 -0400

    Refactor Vehicle class and add displayInfo method
    Removed speed from Vehicle class and updated constructor. Added displayInfo method in Vehicle class.

--- src/transport/Vehicle.java
+++ src/transport/Vehicle.java
@@ -4,21 +4,33 @@ public abstract class Vehicle implements Movable {
 
     protected String brand;
     protected int year;
-    protected int speed;
 
-    public Vehicle(String brand, int year, int speed) {
+    public Vehicle(String brand, int year) {
         this.brand = brand;
         this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand + " Year: " + year);
+    }
+
+}
+class Airplane extends Vehicle {
+    
+    protected int speed;
+
+    public Airplane(String brand, int year, int speed) {
+        super(brand, year);
         this.speed = speed;
+    }
+
+    @Override
+    public void move() {
+        System.out.println("The airplane is flying.");
     }
     
     @Override
     public int getSpeed() {
         return speed;
     }
-
-    public void displayInfo() {
-        System.out.println("Brand: " + brand + " Year: " + year + " Speed: " + speed);
-    }
-
 }
