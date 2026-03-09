22f2d2c9d0b92d5ae2b9077ef5796e913b7aa6ae
sharancp3 <sharancp3@gmail.com>
2026-03-09 16:42:56 -0700

    Bicycle
    Added Bicycle class.

--- src/transport/Vehicle.java
+++ src/transport/Vehicle.java
@@ -13,5 +13,17 @@ public abstract class Vehicle implements Movable {
     public void displayInfo() {
         System.out.println("Brand: " + brand + " Year: " + year);
     }
+    class Bicycle extends Vehicle {
+        public Bicycle (String brand, int year);
+        super (brand, year);
+        int numWheels;
+        this.numWheels = numWheels
+    }
+    class Bicycle implements Movable { 
+        public void move() {
+            System.out.println("This bike travels on the ground by using kinetic energy from humans.")
+                }
+    }
+        
 
 }
